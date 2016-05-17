2da2a6ab562916b1fa4f3c9e24c69533044b07bf
admin <admin@admin.co>
2016-05-17 23:12:15 +0000

    RC1

--- telegraph/app/app/code/local/Sibers/RESTExtender/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Sibers_RESTExtender_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
--- telegraph/app/app/code/local/Sibers/RESTExtender/Model/Api2/Category.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Sibers_RESTExtender_Model_Api2_Category extends Mage_Api2_Model_Resource
-{
-}
