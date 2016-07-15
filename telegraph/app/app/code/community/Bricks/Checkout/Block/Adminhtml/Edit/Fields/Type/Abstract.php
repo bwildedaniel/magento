725e481995ec3a4e0ff22ef3b1b53a7aada176b3
admin <admin@admin.co>
2016-07-15 17:54:38 +0000

    to prod

--- telegraph/app/app/code/community/Bricks/Checkout/Block/Adminhtml/Edit/Fields/Type/Abstract.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Bricks_Checkout_Block_Adminhtml_Edit_Fields_Type_Abstract extends Mage_Adminhtml_Block_Widget {
-	protected $_name = 'abstract';
-}
--- telegraph/app/app/code/community/Bricks/Checkout/Model/Resource/Field/Value.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Bricks_Checkout_Model_Resource_Field_Value extends Mage_Core_Model_Resource_Db_Abstract {
-	protected function _construct() {
-		$this->_init( 'bricks_checkout/field_value', 'field_value_id' );
-	}
-}
